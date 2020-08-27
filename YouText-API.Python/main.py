c72a481771ec92049a9948287f084aa4b9e0d6da
Walter Shewmake <25shewmwalj@washk12.org>
2020-08-27 09:59:00 -0600

    completed python api

--- YouText-API.Python/main.py
+++ YouText-API.Python/main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import socketio
+import json
+
 
 class YouText:
     def __init__(self, api_key, client_secret):
@@ -8,7 +10,7 @@ class YouText:
         self.client_secret = client_secret
         self.sio = None
 
-    def Connect(self, received_message_callback = lambda data: print("[YouText]: The following message was received from " + data.sender + ":\n" + data.message),
+    def Connect(self, received_message_callback = lambda data: print("\n[YouText]: The following message was received from " + data['sender'] + ":\n" + data['message']),
     server = "http://concretegames.net:2345",
     connect_callback = lambda: print("[YouText]: Successfully connected to server."),
     disconnect_callback = lambda: os.execl(sys.executable, os.path.abspath(__file__), *sys.argv)):
@@ -17,6 +19,8 @@ class YouText:
         
         self.sio.connect(server)
         
+        #self.Authenticate()
+
         @self.sio.event
         def connect():
             connect_callback()
@@ -24,32 +28,33 @@ class YouText:
         def disconnect():
             disconnect_callback()
         @self.sio.event
-        def received_message():
-            received_message_callback()
+        def received_message(data):
+            received_message_callback(data)
     def Verify(self):
         api_key = self.api_key
         client_secret = self.client_secret
         self.sio.emit("verify", {
-            api_key: api_key,
-            client_secret: client_secret
+            'api_key': api_key,
+            'client_secret': client_secret
         })
     def Authenticate(self):
         api_key = self.api_key
         client_secret = self.client_secret
         self.sio.emit("verify", {
-            api_key: api_key,
-            client_secret: client_secret
+            'api_key': api_key,
+            'client_secret': client_secret
         })
     def SendMessage(self, message, recipients):
         if (isinstance(recipients, list)):
-           self.sio.emit("send_message", {
+           self.sio.emit("send_messages", {
                 'api_key': self.api_key,
                 'message': message,
                 'recipient': recipients
             })
         else:
-            self.sio.emit("send_messages", {
+            self.sio.emit("send_message", {
                 'api_key': self.api_key,
                 'message': message,
                 'recipient': recipients
             })
+yt = YouText('y0-006-mzqz-ggw2p', '3pexaqvbnx90ddxyp3by7i4xj7lt')
